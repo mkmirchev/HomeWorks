4b7cf489aedef11089678b132a5b1cc6c2c6d331
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 09:24:00 +0300

    Create SqrtRoot.cs

---
+++ IntroToProgramming/SqrtRoot.cs
@@ -0,0 +1,10 @@
+using System;
+
+    class SqrtRoot
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(12345)); // Console prints the square root of the number 12345 by using class math and it's method Sqrt()
+        }
+    }
