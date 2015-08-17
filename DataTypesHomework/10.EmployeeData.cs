c5dba107b462d1422e7befb1cbcb55e9e8fc2588
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 12:17:24 +0300

    Create 10.EmployeeData.cs

---
+++ DataTypesHomework/10.EmployeeData.cs
@@ -0,0 +1,21 @@
+using System;
+
+    class EmployeeData
+    {
+        static void Main()
+        {
+            string firstName = "Amanda";
+            string lastName = "Jonson";
+            byte age = 27;
+            char gender = 'f';
+            long personalID = 8306112507;
+            int employeeID = 27569999;
+            Console.WriteLine("First name: "+firstName);
+            Console.WriteLine("Last name: "+ lastName);
+            Console.WriteLine("Age: "+age);
+            Console.WriteLine("Gender: " + gender);
+            Console.WriteLine("Personal ID number: "+personalID);
+            Console.WriteLine("Unique employee number: "+employeeID);
+        }
+    }
+
