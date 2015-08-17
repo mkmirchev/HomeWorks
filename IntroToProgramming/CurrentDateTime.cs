82afa7290ad8ebb8f0d70a319bf358f77b5c22da
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 09:26:15 +0300

    Create CurrentDateTime.cs

---
+++ IntroToProgramming/CurrentDateTime.cs
@@ -0,0 +1,10 @@
+using System;
+
+    class CurrentDate
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);    // Using the class DateTime and it's property Now
+        }
+    }
+
