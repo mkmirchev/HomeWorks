7d915d869624b08c96a61e7b0f4d07692b28c1ee
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 12:56:09 +0300

    Create 08.NumbersFromOneToN.cs

---
+++ 03.ConsoleInputOutput/08.NumbersFromOneToN.cs
@@ -0,0 +1,16 @@
+using System;
+
+    class NumbersFromOneToN
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+
