a971870f78c9b61adc3f83703f3a2f56edf05ed9
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 09:21:16 +0300

    Create PrintMyName.cs

---
+++ IntroToProgramming/PrintMyName.cs
@@ -0,0 +1,10 @@
+using System;
+
+    class PrintMyName
+    {
+        static void Main()
+        {
+            Console.WriteLine("My name is Mircho."); // Prints my name in the console line.
+        }
+    }
+
