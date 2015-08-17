81e6c1ed691ffcb6a5669420f9ee02ce48ac681b
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 09:23:31 +0300

    Create PrintFullName.cs

---
+++ IntroToProgramming/PrintFullName.cs
@@ -0,0 +1,10 @@
+using System;
+
+    class PrintFullName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mircho");
+            Console.WriteLine("Mirchev");
+        }
+    }
