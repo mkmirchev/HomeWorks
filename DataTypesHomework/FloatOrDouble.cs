d514ac5e9a65b4689c370073c750426f9c603131
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 12:07:23 +0300

    Create FloatOrDouble.cs

---
+++ DataTypesHomework/FloatOrDouble.cs
@@ -0,0 +1,17 @@
+using System;
+
+    class FloatOrDouble
+    {
+        static void Main()
+        {
+            double a = 34.567839023f;
+            float b = 12.345f;
+            double c = 8923.1234857f;
+            float d = 3456.091f;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+
