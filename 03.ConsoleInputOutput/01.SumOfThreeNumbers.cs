22dacfcab19fcbb349994b05909963205dc76f73
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 12:30:48 +0300

    Create 01.SumOfThreeNumbers.cs

---
+++ 03.ConsoleInputOutput/01.SumOfThreeNumbers.cs
@@ -0,0 +1,13 @@
+using System;
+
+    class SumOfThreeNumbers
+    {
+        static void Main()
+        {
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+            decimal c = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(a + b + c);
+        }
+    }
+
