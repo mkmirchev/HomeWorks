ae3a319c0415fa0420828d196f206193dcd889aa
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 12:15:48 +0300

    Create 07.QuotesInStrings.cs

---
+++ DataTypesHomework/07.QuotesInStrings.cs
@@ -0,0 +1,14 @@
+using System;
+
+    class QuotesInStrings
+    {
+        static void Main()
+        {
+
+            string a = "The \"use\" of quotation causes difficulties";
+            string b = "The use of quotation causes difficulties";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+
