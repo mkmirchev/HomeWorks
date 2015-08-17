76c98440ec0824003f430adef73547cd6c7bb761
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 12:17:47 +0300

    Create 11.BankAccount.cs

---
+++ DataTypesHomework/11.BankAccount.cs
@@ -0,0 +1,19 @@
+using System;
+
+    class BankAccount
+    {
+
+        static void Main()
+        {
+            string firstName;
+            string secondName;
+            string lastName;
+            double accBalance;
+            string bankName;
+            long iban;
+            long firstCreditCard;
+            long SecondCreditCard;
+            long ThirdCreditCard;
+        }
+    }
+
