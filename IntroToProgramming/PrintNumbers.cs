4ea384adb8ec1f1282afb7a0d99c3e997a37309a
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 09:23:00 +0300

    Create PrintNumbers.cs

---
+++ IntroToProgramming/PrintNumbers.cs
@@ -0,0 +1,12 @@
+using System;
+
+    class PrintNumbers
+    {
+        static void Main()
+        {
+            Console.WriteLine(1);       //Print the number 1 in the console.
+            Console.WriteLine(101);     //Print the number 101 in the console.
+            Console.WriteLine(1001);    //Print the number 1001 in the console.
+        }
+    }
+
