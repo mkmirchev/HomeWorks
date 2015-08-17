de0d5e13bcf1302807bed97094ec6bdd69f025c8
Mircho Mirchev <mkmirchev@abv.bg>
2015-08-17 13:09:06 +0300

    Create 09.Trapezoidz.cs

---
+++ 04.OperatorsExpressionsStatements/09.Trapezoidz.cs
@@ -0,0 +1,14 @@
+using System;
+
+    class Trapezoidz
+    {
+        static void Main()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double area = ((a + b) * h) / 2;
+            Console.WriteLine(area);
+        }
+    }
+
